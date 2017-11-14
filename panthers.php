799dae71ecf40f2f10688fb532345fbba287cea5
Igor <igor@devfire.io>
2017-11-14 18:25:06 -0500

    near finished

--- panthers.php
+++ panthers.php
@@ -3,8 +3,7 @@
 </head>
 <body>
 <?php
-  echo "Hello world!";
-  $array = array("Tom", "Dick", "Harry");
+  //define the initial array
   $receivers = array(
     array("McCaffrey", 406),
     array("Funchess", 443),
@@ -17,10 +16,18 @@
     array("Manhertz",	11),
     array("Artis-Payne", 2)
   );
-
+  echo "<h1>Carolina Panthers Top Receivers</h1>";
+  echo "<table>";
   for ($row=0; $row<count($receivers); $row++) {
-    print $receivers[$row][0]
-  }
+    $yardage = $receivers[$row][1];
+    $name = $receivers[$row][0];
+    if ($yardage > 99) {
+        echo "<tr>";
+        echo "<td>". $name. "</td><td>".$yardage."</td>";
+        echo "</tr>";
+    }
+  };
+  echo "</table>";
 ?>
 </body>
 </html>
