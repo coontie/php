17bfd4fd687b693e3795faffd9caa6d5954111e8
Igor <igor@devfire.io>
2017-11-14 18:05:29 -0500

    added the array

--- panthers.php
+++ panthers.php
@@ -4,6 +4,19 @@
 <body>
 <?php
   echo "Hello world!";
+  $array = array("Tom", "Dick", "Harry");
+  $receivers = array(
+    array("McCaffrey", 406),
+    array("Funchess", 443),
+    array("Dickson", 349),
+    array("Shepard", 133),
+    array("Samuel",	70),
+    array("Stewart", 44),
+    array("Whittaker", 34),
+    array("Olsen", 28),
+    array("Manhertz",	11),
+    array("Artis-Payne", 2)
+  );
 ?>
 </body>
 </html>
