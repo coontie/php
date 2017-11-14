e9d4042e58fa8492104091c6bd9d82b44979fcad
Igor <igor@devfire.io>
2017-11-14 18:33:05 -0500

    final commit

--- panthers.php
+++ panthers.php
@@ -5,15 +5,16 @@
 <?php
   //define the initial array
   $receivers = array(
-    array("McCaffrey", 406),
-    array("Funchess", 443),
-    array("Dickson", 349),
-    array("Shepard", 133),
-    array("Samuel",	70),
+    array("McCaffrey", 433),
+    array("Funchess", 535),
+    array("Dickson", 382),
+    array("Shepard", 173),
+    array("Samuel",	115),
     array("Stewart", 44),
     array("Whittaker", 34),
     array("Olsen", 28),
     array("Manhertz",	11),
+    array("Clay", 5),
     array("Artis-Payne", 2)
   );
   echo "<h1>Carolina Panthers Top Receivers</h1>";
@@ -21,7 +22,8 @@
   for ($row=0; $row<count($receivers); $row++) {
     $yardage = $receivers[$row][1];
     $name = $receivers[$row][0];
-    if ($yardage > 99) {
+    $maxYardage = 99;
+    if ($yardage > $maxYardage) {
         echo "<tr>";
         echo "<td>". $name. "</td><td>".$yardage."</td>";
         echo "</tr>";
