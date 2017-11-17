f68c50aef418859b538d748ff9d93a667aba9157
Igor <igor@devfire.io>
2017-11-17 18:55:48 -0500

    initial commit

---
+++ form.php
@@ -0,0 +1,19 @@
+<html>
+<head><title>Carolina Panthers RB Stats</title>
+</head>
+<body>
+  <form action="thankyou.php" method="post">
+    <input type="text" name="f_name">
+    <input type="text" name="l_name">
+    <select name="position">
+      <option value="qb">Quarterback</option>
+      <option value="rb">Running Back</option>
+      <option value="ol">Offensive Line/option>
+      <option value="wr">Wide Receiver</option>
+      <option value="te">Tight End</option>
+    </select>
+    <input type="text" name="rush_yards">
+    <input type="text" name="rec_yards">
+  </form>
+</body>
+</html>
