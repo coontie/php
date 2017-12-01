b4b09c17f0f9d64b66a252b71e8e5e5378617e00
coontie <coontie@gmail.com>
2017-11-30 21:53:06 -0500

    added sql_to_html_table

--- viewplayers.php
+++ viewplayers.php
@@ -3,7 +3,36 @@
 <body>
   <h1>View all players</h1>
 <?php
+  
+  function sql_to_html_table($sqlresult, $delim="\n") {
+  // starting table
+  $htmltable =  "<table>" . $delim ;   
+  $counter   = 0 ;
+  // putting in lines
+  while( $row = $sqlresult->fetch_assoc()  ){
+    if ( $counter===0 ) {
+      // table header
+      $htmltable .=   "<tr>"  . $delim;
+      foreach ($row as $key => $value ) {
+          $htmltable .=   "<th>" . $key . "</th>"  . $delim ;
+      }
+      $htmltable .=   "</tr>"  . $delim ; 
+      $counter = 22;
+    } 
+      // table body
+      $htmltable .=   "<tr>"  . $delim ;
+      foreach ($row as $key => $value ) {
+          $htmltable .=   "<td>" . $value . "</td>"  . $delim ;
+      }
+      $htmltable .=   "</tr>"   . $delim ;
+  }
+  // closing table
+  $htmltable .=   "</table>"   . $delim ; 
+  // return
+  return( $htmltable ) ; 
+}
 
+  
   $connection = mysqli_connect("localhost", "f17d", "Pirates4Ever", "f17d");
 
   $sql = "select * from players";
@@ -18,6 +47,7 @@
       echo $player['RushYards'];
   }
 
+  echo sql_to_html_table( $player_set, $delim="\n" ) ; 
   mysqli_free_result($player_set);
 
   mysqli_close($connection);
